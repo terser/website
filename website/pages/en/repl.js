d822c95ffcea1b4c9820a52a9308138dcb114212
rogeriopvl <rogeriopvl@gmail.com>
2019-08-22 15:59:36 +0100

    feat: implement the repl page

--- website/pages/en/repl.js
+++ website/pages/en/repl.js
@@ -1,33 +1,19 @@
 const React = require('react');
 
-const CompLibrary = require('../../core/CompLibrary.js');
+const iframeStyle = {
+  margin: 0,
+  padding: 0,
+  border: 'none',
+  width: '100%',
+  height: '100%',
+}
 
-const Container = CompLibrary.Container;
-
-class Users extends React.Component {
+class Repl extends React.Component {
   render() {
-    const {config: siteConfig} = this.props;
-    if ((siteConfig.users || []).length === 0) {
-      return null;
-    }
-
-    const editUrl = `${siteConfig.repoUrl}/edit/master/website/siteConfig.js`;
-    const showcase = siteConfig.users.map(user => (
-      <a href={user.infoLink} key={user.infoLink}>
-        <img src={user.image} alt={user.caption} title={user.caption} />
-      </a>
-    ));
-
     return (
-      <div className="mainContainer">
-        <Container padding={['bottom', 'top']}>
-          <div className="replSection">
-            soon...
-          </div>
-        </Container>
-      </div>
+      <iframe src='https://try.terser.org' style={iframeStyle} />
     );
   }
 }
 
-module.exports = Users;
+module.exports = Repl;

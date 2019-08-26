944257fb26c245274e89d93b40da46d66ef1a8da
rogeriopvl <rogeriopvl@gmail.com>
2019-08-26 15:15:52 +0100

    feat: add repl page

--- website/pages/en/repl.js
+++ website/pages/en/repl.js
@@ -1,17 +1,21 @@
-const React = require('react');
+const React = require("react");
 
 const iframeStyle = {
   margin: 0,
   padding: 0,
-  border: 'none',
-  width: '100%',
-  height: '100%',
-}
+  border: "none",
+  width: "100%",
+  height: "100%"
+};
 
 class Repl extends React.Component {
   render() {
     return (
-      <iframe src='https://try.terser.org' style={iframeStyle} />
+      <iframe
+        sandbox="allow-scripts"
+        src="https://try.terser.org"
+        style={iframeStyle}
+      />
     );
   }
 }

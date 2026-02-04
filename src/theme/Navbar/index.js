9d40038fe12bc2cf2e9fc0ff6000167144d2f2cc
Fábio Santos <fabiosantosart@gmail.com>
2026-02-04 12:42:56 +0000

    add notice and link regarding Minnesota

---
+++ src/theme/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NavbarLayout from '@theme-original/Navbar/Layout';
+import NavbarContent from '@theme-original/Navbar/Content';
+import styles from './index.module.css';
+
+export default function Navbar() {
+  return (
+    <>
+      <a href="https://www.standwithminnesota.com/" target="__blank" className={styles.topBar}>
+        <span>
+          <strong>Minnesotans are being repressed by a violent regime</strong>
+          <br />
+          <u>Find out how you can help help!</u>
+        </span>
+      </a>
+      <NavbarLayout>
+        <NavbarContent />
+      </NavbarLayout>
+    </>
+  );
+}
